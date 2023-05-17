c281481e99c4c95cbca27c5659239871b90cd230
Tyler Sanders <tsanders5902@gmail.com>
2023-05-17 11:28:14 -0400

    Initial commit

---
+++ src/components/projectGallery/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+const ProjectGallery = () => {
+
+return (
+    <div>
+        yeah look at the-barchive.netlify.app
+        <div className="barchivePreview">
+        
+        </div>
+    </div>
+);
+
+}
+
+
+
+export default ProjectGallery;
